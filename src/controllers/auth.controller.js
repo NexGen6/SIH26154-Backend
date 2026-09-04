47575e3bb5348c2b1bf4e4eb98c1d804e2ef866f
NexGen6 <nexgensix@gmail.com>
2026-09-05 02:16:13 +0530

    login added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -44,7 +44,47 @@ async function registerUser(req, res) {
      });
 }
 
+async function loginUser(req, res) {
+
+     const { email , password } =  req.body ;
+
+     const user = await userModel.findOne({ email });
+
+     if(!user) {
+          return res.status(401).json({
+               message:"Invalid Email or Password"
+          });
+     }
+
+     const isPasswordCorrect = await bcrypt.compare(password , user.password);
+
+     if(!isPasswordCorrect) {
+          return res.status(401).json({
+               message: "Invalid Email or Password"
+          });
+     }
+
+     const token = jwt.sign({id: user._id,}, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+
+     res.cookie("token", token , {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict"
+     });
+
+     res.status(200).json({
+          message: "Login Successfull",
+          user: {
+               _id: user._id,
+               name: user.name,
+               email: user.email,
+          }
+     });
+}
+
 
 module.exports = {
-     registerUser
+     registerUser,
+     loginUser
 }
